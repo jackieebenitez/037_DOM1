5c123631dcbaf0a9a95ea5950403ad68e1fa49e0
jackieebenitez <jackieebenitez@gmail.com>
2016-10-19 14:14:58 +0000

    Hice cambios al JS, comente mi código y quite líneas de código

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -1,34 +1,33 @@
+//recuerda que en el HTML usaste /required que es una palabra
+//reservada que se puede usar para el html y que te dice que los
+//espacios son obligatorios. 
 function validateForm(){
+	// a continuacióne están las expresiones regulares, son como
+	//operaciones que se mandan llamar en los if
 	var regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
 	var regexNombre = /^[A-Z][a-z]*$/;
-	var regexPassword = /^\d{9}$/;
-	
-	var name = document.getElementById("name").value // DONE
-	var lastname = document.getElementById ("lastname").value; //DONE 
-	var mail = document.getElementById ("input-email").value; //DONE
+	// hago las variables y las mando llamar del html
+	var name = document.getElementById("name").value
+	var lastname = document.getElementById ("lastname").value;
+	var mail = document.getElementById ("input-email").value; 
 	var password = document.getElementById("input-password").value;
-	var mensaje = " ";
-	var errores = true; 
-
+	//inician mis condiciones
+	//si regexNombre es diferente a lo que hace la expresión regular
+	//entonces manda un alert que diga ... y así con cada uno. 
 	if (!regexNombre.test(name)){
-		alert("está mal nombre");
+		alert("La primer letra de tu nombre debe ser mayúscula");
 	}
 	if (!regexNombre.test(lastname)){
-		mensaje += "está mal apellido";
-		errores = false;
+		alert("La primer letra de tu apellido debe ser mayúscula");
+		//errores = false;
 	}
 	if(!regexEmail.test(mail)){
-		mensaje += "está mal email";
-		errores = false;  
+		alert("Tu e-mail es incorrecto");
 	}
-	if(!regexPassword<6){
-		mensaje = "está mal password son 6 letras";
-		errores = false;
+	if(password.length < 6){
+		alert("Tu password debe contener al menos 6 caracteres");
 	}
-	if(!errores){
-		alert(mensaje);
-	} 
-	return errores;
+	
 }
 
 	
